3d7e51df9bc65884edd935b5d131278a31da3e93
subham <saisubhamsahoo091@gmail.com>
2026-09-13 10:58:24 +0530

    Adding a Footer in my website

---
+++ my-app/src/Components/Footer.jsx
@@ -0,0 +1,22 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <h2>
+        Sai<span>.</span>
+      </h2>
+
+      <p>
+        © 2026 Sai Subham Sahoo. All Rights Reserved.
+      </p>
+
+      <div className="footer-links">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
